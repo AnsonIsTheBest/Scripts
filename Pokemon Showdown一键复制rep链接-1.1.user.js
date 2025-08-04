fc4f6adb8f8c9ce5094450e44c7b71259d43fff8
AnsonIsTheBest <ansonuisutheubest@gmail.com>
2025-08-03 22:33:09 -0700

    Update Pokemon Showdown一键复制rep链接-1.1.user.js

--- Pokemon Showdown一键复制rep链接-1.1.user.js
+++ Pokemon Showdown一键复制rep链接-1.1.user.js
@@ -1,20 +1,18 @@
 // ==UserScript==
 // @name         Pokemon Showdown一键复制rep链接
-// @name:en      Pokémon Showdown Replay Link Exporter
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  在pokemon showdown replay添加一个按钮用于一键复制所有回放链接以便复制到replay scouter中
-// @description:en  Added a button in pokemon showdown replay page to copy all the replay links to paste in the replay scouter
-// @author       AnsonIsTheBest
+// @author       You
 // @match        https://replay.pokemonshowdown.com/*
 // @grant        GM_setClipboard
 // @grant        GM_addStyle
 // @license      WTFPL
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     GM_addStyle(`
         .export-replay-button {
             background-color: #4CAF50;
@@ -24,45 +22,45 @@
             border-radius: 5px;
             cursor: pointer;
             font-size: 14px;
-            margin-left: 15px;
-            vertical-align: middle;
+            margin-left: 15px; 
+            vertical-align: middle; 
         }
         .export-replay-button:hover {
             background-color: #45a049;
         }
     `);
-
+ 
     const checkInterval = setInterval(() => {
         const targetElement = document.querySelector('.main h1');
         if (targetElement && !document.querySelector('.export-replay-button')) {
             clearInterval(checkInterval);
-
+ 
             let exportButton = document.createElement('button');
-            exportButton.innerHTML = '复制所有回放链接Copy all replay links';
+            exportButton.innerHTML = '复制所有回放链接';
             exportButton.className = 'export-replay-button';
-
+ 
             targetElement.appendChild(exportButton);
-
+ 
             exportButton.addEventListener('click', () => {
                 const linkElements = document.querySelectorAll('ul.linklist a.blocklink');
                 if (linkElements.length > 0) {
                     const links = Array.from(linkElements).map(a => a.href);
                     const linksText = links.join('\n');
-
+ 
                     GM_setClipboard(linksText, 'text');
-
-                    exportButton.innerHTML = `已复制 ${links.length} 条链接！Copied ${links.length} links!`;
+ 
+                    exportButton.innerHTML = `已复制 ${links.length} 条链接！`;
                     setTimeout(() => {
-                        exportButton.innerHTML = '复制所有回放链接Copy all replay links';
-                    }, 2000);
+                        exportButton.innerHTML = '复制所有回放链接';
+                    }, 2000); 
                 } else {
-                    exportButton.innerHTML = '未找到链接Did not find any links';
+                    exportButton.innerHTML = '未找到链接';
                      setTimeout(() => {
-                        exportButton.innerHTML = '复制所有回放链接Copy all replay links';
+                        exportButton.innerHTML = '复制所有回放链接';
                     }, 2000);
                 }
             });
         }
     }, 500);
-
+ 
 })();
